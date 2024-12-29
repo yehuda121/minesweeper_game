9d9652a2fc4ca2af9bb0fa14eb704731afe2973d
Yehuda <yehudash1994@gmail.com>
2024-12-29 23:27:52 +0200

    Initial upload of Minesweeper game

---
+++ getch.h
@@ -0,0 +1,10 @@
+#ifndef GETCH
+#define GETCH
+
+// //hangs untill a key is pressed and returns its character, without waiting for '\n', and without echo (without printing the character to the screen).
+// char getch(void);
+// //same as getch, but also echos the character to the screen.
+// char getche(void);
+char my_getch(void);
+char my_getche(void);
+#endif
